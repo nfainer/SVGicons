cb0dfb7cd33f479a3fd543f7ea370c441889c748
Noah Fainer <noahfainer@macbook-pro-3.fc.ca>
2018-02-15 10:35:57 -0500

    svg stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 (() => {
-  console.log('fired!');
+  console.log('fired! JS is FUN!!');
+//variabes at the top
+  var iconSet = document.querySelectorAll('svg');
+//functions here
+function logThisIcon() {
+  console.log('clicked on this icon ', this.id);
+
+  this.style.opacity = 0.5;
+}
+
+//event handling
+  iconSet.forEach(icon => icon.addEventListener('click',
+  logThisIcon));
 })();
